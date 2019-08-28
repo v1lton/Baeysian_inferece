dff84d454c30f2dccefa4d309b56c0b864399046
v1lton <wrs@cin.ufpe.br>
2019-08-28 17:12:52 -0300

    creating lower_case_documents and sans_punctuation_documents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 df = pd.read_table('SMSSpamCollection', sep='\t', header=None, names=["label", "sms_message"])
 print(df.head(5))
@@ -6,3 +7,18 @@ print(df.head(5))
 df['label'].replace('ham', 0, inplace=True)
 df['label'].replace('spam', 1, inplace=True)
 print(df.shape)
+
+documents = ['Hello, how are you!',
+             'Win money, win from home.',
+             'Call me now.',
+             'Hello, Call hello you tomorrow?']
+lower_case_documents = []
+i = 0
+for x in documents:
+    lower_case_documents.append(x.lower())
+print(lower_case_documents)
+
+sans_punctuation_documents = []
+for i in lower_case_documents:
+    sans_punctuation_documents.append(re.sub(r'[^\w\s]','',i))
+print(sans_punctuation_documents)
